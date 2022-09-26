fe4556018b84cb282ebfc09a9dcbf1bce9d03e4d
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-09-26 21:08:37 +0600

    add style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import "./App.css";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold no-underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen gap-8">
+        <div className=" text-4xl text-neutral-700">
+          <h2>POMODORO</h2>
+        </div>
+        <div className="text-9xl text-neutral-700">
+          <span>00</span>
+          <span>:</span>
+          <span>00</span>
+        </div>
+        <div className="flex flex-row  gap-4 font-semibold text-gray-800">
+          <button className="bg-yellow-200 hover:bg-yellow-300 py-2 px-4 rounded-md text-lg">
+            Stop
+          </button>
+          <button className="bg-yellow-200 hover:bg-yellow-300 py-2 px-4 rounded-md">
+            Start
+          </button>
+          <button className="bg-yellow-200 hover:bg-yellow-300 py-2 px-4 rounded-md">
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
